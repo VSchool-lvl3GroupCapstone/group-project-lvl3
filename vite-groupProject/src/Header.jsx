0b1895960b09f02856d531056f17f9850841e1da
mathew macias <mattmacias.mm@gmail.com>
2023-04-13 12:39:20 -0700

    Header built

---
+++ vite-groupProject/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <header>
+            <img src="https://www.clipartmax.com/png/small/101-1015783_el-burrito-by-azure-arts-burrito-with-mustache.png"/>
+            <h2>Absurd Memes</h2>
+            <h4>Meme Generator</h4>
+            
+        </header>
+    )
+}
